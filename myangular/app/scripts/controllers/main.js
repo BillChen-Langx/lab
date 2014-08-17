233a1524c8b3c5085bb0ba82be235ce04664f95e
Bill Chen <drhbchen@gmail.com>
2014-08-16 22:06:17 -0700

    Used element.trigger('focus') in the controller to focus
    Directly call element.trigger('focus') against the target field.
    This is simple but we're manipulating DOM in the controller?

--- myangular/app/scripts/controllers/main.js
+++ myangular/app/scripts/controllers/main.js
@@ -23,25 +23,35 @@ angular.module('myangularApp')
         $scope.clickCount = 0;
         $scope.onTestBtnClick = function(){
             countClicks();
-
-            var statusName = 'input'+ $scope.clickCount;
-            $scope.focusStatus[statusName] = true;
-            for(var property in $scope.focusStatus){
-                if(property !== statusName){
-                    $scope.focusStatus[property] = false;
-                }
-            }
-
-           // console.log('$scope.focusStatus.input' + $scope.clickCount + '=' +
-         //       $scope.focusStatus[statusName]);
+            var fieldIdName = 'input'+ $scope.clickCount;
+            //setFocusViaDirective(fieldIdName);
+            setFocusViaJQuery(fieldIdName);
         };
 
         var countClicks = function(){
-            if($scope.clickCount >= 8){
+            if($scope.clickCount >= 9){
               $scope.clickCount = 1;
             } else {
                 $scope.clickCount++;
             }
+        };
+//
+//        var setFocusViaDirective = function(fieldIdName){
+//            $scope.focusStatus[fieldIdName] = true;
+//            for(var property in $scope.focusStatus){
+//                if(property !== fieldIdName){
+//                    $scope.focusStatus[property] = false;
+//                }
+//            }
+//        };
+
+        var setFocusViaJQuery = function(fieldIdName){
+            console.log('fieldIdName=' + fieldIdName);
+            //all works. has to use "id". "name" won't work.
+            //document.getElementById(fieldIdName).focus();
+            //angular.element('#'+fieldIdName).trigger('focus') ;
+
+              $('#'+ fieldIdName).trigger('focus');
         };
 
         $scope.title = 'Test Page';
